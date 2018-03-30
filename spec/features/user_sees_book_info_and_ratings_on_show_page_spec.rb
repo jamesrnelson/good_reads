40fab8ce77bb04e8eeb8395192e784a16e629689
JNelson <nelson.jimmy@gmail.com>
2018-03-30 10:34:43 -0600

    created Review model that belongs to book and user. Added validation tests

--- spec/features/user_sees_book_info_and_ratings_on_show_page_spec.rb
+++ spec/features/user_sees_book_info_and_ratings_on_show_page_spec.rb
@@ -4,11 +4,14 @@ describe 'User' do
   context 'goes to book show page' do
     it 'should see the information and ratings of that book' do
       book1 = Book.create!(title: 'War and Peace')
+      user1 = User.create!(name: 'Chekhov')
+      user1.reviews.create!(body: 'This is a really long book. I prefer plays.', rating: 1)
 
       visit book_path(book1)
 
       expect(page).to have_content('War and Peace')
-      expect(page).to have_content('War and Peace')
+      expect(page).to have_content('Chekhov')
+      expect(page).to have_content('1')
     end
   end
 end
