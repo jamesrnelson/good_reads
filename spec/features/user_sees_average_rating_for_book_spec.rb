11643b899d68538fcec8110892554b32ad491449
JNelson <nelson.jimmy@gmail.com>
2018-03-30 10:49:12 -0600

    shows average rating

--- spec/features/user_sees_average_rating_for_book_spec.rb
+++ spec/features/user_sees_average_rating_for_book_spec.rb
@@ -4,14 +4,14 @@ describe 'User' do
   context 'goes to book show page' do
     it 'should display the average rating' do
       book1 = Book.create!(title: 'War and Peace')
+      user2 = User.create!(name: 'Dostoevsky')
+      user2.reviews.create!(body: 'This book is mediocre. I could do better', rating: 3, book: book1)
       user1 = User.create!(name: 'Chekhov')
       user1.reviews.create!(body: 'This is a really long book. I prefer plays.', rating: 1, book: book1)
 
-      book1 = Book.create!(title: 'War and Peace')
-      user2 = User.create!(name: 'Dostoevsky')
-      user2.reviews.create!(body: 'This book is mediocre. I could do better', rating: 3, book: book1)
 
       visit book_path(book1)
+      save_and_open_page
 
       expect(page).to have_content('Average Rating: 2')
     end
